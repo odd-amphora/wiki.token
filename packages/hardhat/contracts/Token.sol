7eb14248c0c8c6f9e873dd2be508afef04f383ee
bingbongle <contact@defund12.org>
2021-02-06 18:40:28 -0500

    Refactor token to use wikidataId to simplify things and save memory

--- packages/hardhat/contracts/Token.sol
+++ packages/hardhat/contracts/Token.sol
@@ -3,19 +3,15 @@ pragma solidity >=0.6.0 <0.7.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract Token is ERC721, Ownable {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+  // The number of tokens each address has currently minted
+  mapping (address => uint16) private _mintedTokensPerAddress;
 
-  // The number of tokens each user has currently minted
-  mapping (address => uint16) private _mintedTokensPerUser;
-
-  // Maps a page to a token
-  mapping(string => uint256) private _pageToTokenId;
+  // Maps a wikidataId to an address
+  mapping (uint256 => address) private _wikidataIdToAddress;
 
   constructor () public ERC721("WikiToken", "WIKI") {
     // TODO(teddywilson) this will be replaced by a homegrown API.
@@ -30,26 +26,21 @@ contract Token is ERC721, Ownable {
     return 16;
   }
 
-  function mint(string memory page) public {
-    bytes memory pageBytes = bytes(page);
-    require (pageBytes.length > 0, "Page must not be empty");
-    // Adhere to wikipedia case sensitivity rules
-    require(
-      (uint8(pageBytes[0]) < uint8(65)) || (uint8(pageBytes[0]) > uint8(90)),
-      "First character of page must be lowercase"
-    );   
-    require (_pageToTokenId[page] == 0, "Page must not be claimed");
+  function isClaimed(uint256 wikidataId) view public returns (bool) {
+    return _wikidataIdToAddress[wikidataId] != 0x0000000000000000000000000000000000000000;
+  }
+
+  function mint(uint256 wikidataId) public {
+    require (!isClaimed(wikidataId), "Page must not be claimed");
     require (
-      _mintedTokensPerUser[msg.sender] < getMaxMintableTokensPerUser(),
+      _mintedTokensPerAddress[msg.sender] < getMaxMintableTokensPerUser(),
       "Max minted tokens reached"
     );
 
-    _tokenIds.increment();
-    uint256 newTokenId = _tokenIds.current();
-    _mint(msg.sender, newTokenId);
-    _setTokenURI(newTokenId, page);
+    _mint(msg.sender, wikidataId);
+    _setTokenURI(wikidataId, Strings.toString(wikidataId));
 
-    _pageToTokenId[page] = newTokenId;
-    _mintedTokensPerUser[msg.sender] += 1;
+    _wikidataIdToAddress[wikidataId] = msg.sender;
+    _mintedTokensPerAddress[msg.sender] += 1;
   }
 }
--- packages/hardhat/test/Token.js
+++ packages/hardhat/test/Token.js
@@ -1,9 +1,8 @@
 const { expect } = require("chai");
-const BigNumber = require("bignumber.js");
 
 describe("Token Contract", function () {
-  let WikiToken;
-  let hardhatWikiToken;
+  let Token;
+  let hardhatToken;
   let owner;
   let addr1;
   let addr2;
@@ -16,38 +15,40 @@ describe("Token Contract", function () {
     hardhatToken = await Token.deploy();
   });
 
-  describe("Mint", function () {
-    it("Should not mint empty pages", async function () {
-      await expect(hardhatToken.mint()).to.be.reverted;
-      await expect(hardhatToken.mint(0)).to.be.reverted;
-      await expect(hardhatToken.mint("")).to.be.reverted;
-    });
-
-    it("Should not mint pages with first character uppercase", async function () {
-      await expect(hardhatToken.mint("Klein_bottle")).to.be.reverted;
-      await expect(hardhatToken.mint("Möbius_strip")).to.be.reverted;
-      await expect(hardhatToken.mint("Hopf–Rinow_theorem")).to.be.reverted;
-      // Validate lowercase passes
-      await expect(hardhatToken.mint("klein_bottle")).to.not.be.reverted;
-      await expect(hardhatToken.mint("möbius_strip")).to.not.be.reverted;
-      await expect(hardhatToken.mint("hopf–Rinow_theorem")).to.not.be.reverted;
-    });
-
+  describe("mint()", function () {
     it("Should not mint pages that have already been claimed", async function () {
-      await hardhatToken.mint("satoshi_Nakamoto");
-      await expect(hardhatToken.mint("satoshi_Nakamoto")).to.be.reverted;
+      await hardhatToken.mint(1);
+      await expect(hardhatToken.mint(1)).to.be.reverted;
 
-      await hardhatToken.mint("ryokan_(inn)");
-      await expect(hardhatToken.mint("ryokan_(inn)")).to.be.reverted;
+      await hardhatToken.mint(2);
+      await expect(hardhatToken.mint(2)).to.be.reverted;
     });
 
     it("Should limit number of mintable tokens per user", async function () {
       const maxMintableTokensPerUser = await hardhatToken.getMaxMintableTokensPerUser();
       for (var i = 1; i <= maxMintableTokensPerUser; i++) {
-        var page = "some_page".concat(i);
-        await expect(hardhatToken.mint(page)).to.not.be.reverted;
+        await expect(hardhatToken.mint(i)).to.not.be.reverted;
       }
-      await expect(hardhatToken.mint("ryokan_(inn)")).to.be.reverted;
+      await expect(hardhatToken.mint(maxMintableTokensPerUser + 1)).to.be.reverted;
+    });
+  });
+
+  describe("isClaimed()", function () {
+    it("Should return true if already claimed, false otherwise", async function () {
+      await hardhatToken.mint(1);
+      expect(await hardhatToken.isClaimed(1)).to.be.true;
+
+      await hardhatToken.mint(2);
+      expect(await hardhatToken.isClaimed(2)).to.be.true;
+
+      await hardhatToken.mint(3);
+      expect(await hardhatToken.isClaimed(3)).to.be.true;
+
+      expect(await hardhatToken.isClaimed(4)).to.be.false;
+      expect(await hardhatToken.isClaimed(5)).to.be.false;
+      expect(await hardhatToken.isClaimed(6)).to.be.false;
+      expect(await hardhatToken.isClaimed(7)).to.be.false;
+      expect(await hardhatToken.isClaimed(8)).to.be.false;
     });
   });
 });
