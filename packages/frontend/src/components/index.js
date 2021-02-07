737576c19b1e7a672dba385b9f41f7ec52cfab5b
bingbongle <contact@defund12.org>
2021-02-07 13:31:18 -0500

    Update index

--- packages/frontend/src/components/index.js
+++ packages/frontend/src/components/index.js
@@ -1,17 +1,1 @@
-export { default as Account } from "./Account";
-export { default as Contract } from "./Contract";
-export { default as Address } from "./Address";
-export { default as AddressInput } from "./AddressInput";
-export { default as EtherInput } from "./EtherInput";
-export { default as Balance } from "./Balance";
-export { default as Provider } from "./Provider";
-export { default as Ramp } from "./Ramp";
-export { default as Faucet } from "./Faucet";
-export { default as Wallet } from "./Wallet";
-export { default as Blockie } from "./Blockie";
-export { default as Header } from "./Header";
-export { default as Timeline } from "./Timeline";
-export { default as GasGauge } from "./GasGauge";
-
-// Custom
 export { default as ClaimToken } from "./ClaimToken";
