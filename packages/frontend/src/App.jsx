0efe75cc188d847b90883918869898943f026052
bingbongle <contact@defund12.org>
2021-02-07 18:06:38 -0500

    Shave App.jsx

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -97,17 +97,6 @@ function App(props) {
   const writeContracts = useContractLoader(userProvider);
   if (DEBUG) console.log("🔐 writeContracts", writeContracts);
 
-  // EXTERNAL CONTRACT EXAMPLE:
-  //
-  // If you want to bring in the mainnet DAI contract it would look like:
-  //const mainnetDAIContract = useExternalContractLoader(mainnetProvider, DAI_ADDRESS, DAI_ABI)
-  //console.log("🥇DAI contract on mainnet:",mainnetDAIContract)
-  //
-  // Then read your DAI balance like:
-  //const myMainnetBalance = useContractReader({DAI: mainnetDAIContract},"DAI", "balanceOf",["0x34aA3F359A9D614239015126635CE7732c18fDF3"])
-  //console.log("💲 myMainnetBalance:",myMainnetBalance)
-  //
-
   // keep track of a variable from the contract in the local React state:
   const purpose = useContractReader(readContracts, "YourContract", "purpose");
   console.log("🤗 purpose:", purpose);
