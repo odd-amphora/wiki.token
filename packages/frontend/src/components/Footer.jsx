fc1321ded71849c220ac4f3d6954c5630c81526a
bingbongle <contact@defund12.org>
2021-02-13 16:30:31 -0500

    Add social icons to footer

--- packages/frontend/src/components/Footer.jsx
+++ packages/frontend/src/components/Footer.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { Layout } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faDiscord, faMedium, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
+const FA_ICON_SIZE = "lg";
+
+// TODO(teddywilson): Make icons clickable
 export default function Footer() {
-  return <Layout.Footer style={{ textAlign: "center" }}>wiki.token ©2021</Layout.Footer>;
+  return (
+    <Layout.Footer style={{ textAlign: "center" }}>
+      <div>wiki.token ©2021</div>
+      <div style={{ display: "inline-flex", justifyContent: "center" }}>
+        <FontAwesomeIcon icon={faGithub} className="footer-icon" size={FA_ICON_SIZE} />
+        <FontAwesomeIcon icon={faMedium} className="footer-icon" size={FA_ICON_SIZE} />
+        <FontAwesomeIcon icon={faTwitter} className="footer-icon" size={FA_ICON_SIZE} />
+        <FontAwesomeIcon icon={faDiscord} className="footer-icon" size={FA_ICON_SIZE} />
+      </div>
+    </Layout.Footer>
+  );
 }
