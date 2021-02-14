dc89ce3709fbd5b04340c635b4e8738fcf4165e3
bingbongle <contact@defund12.org>
2021-02-14 15:28:19 -0500

    Make socials clickable

--- packages/frontend/src/components/Footer.jsx
+++ packages/frontend/src/components/Footer.jsx
@@ -7,14 +7,47 @@ const FA_ICON_SIZE = "lg";
 
 // TODO(teddywilson): Make icons clickable
 export default function Footer() {
+  function openSocialLink(link) {
+    window.open(link);
+  }
+
   return (
     <Layout.Footer style={{ textAlign: "center" }}>
       <div>wiki.token ©2021</div>
       <div style={{ display: "inline-flex", justifyContent: "center" }}>
-        <FontAwesomeIcon icon={faGithub} className="footer-icon" size={FA_ICON_SIZE} />
-        <FontAwesomeIcon icon={faMedium} className="footer-icon" size={FA_ICON_SIZE} />
-        <FontAwesomeIcon icon={faTwitter} className="footer-icon" size={FA_ICON_SIZE} />
-        <FontAwesomeIcon icon={faDiscord} className="footer-icon" size={FA_ICON_SIZE} />
+        <FontAwesomeIcon
+          icon={faGithub}
+          className="footer-icon"
+          size={FA_ICON_SIZE}
+          onClick={() => {
+            openSocialLink(`https://github.com/wiki-token`);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faMedium}
+          className="footer-icon"
+          size={FA_ICON_SIZE}
+          // TODO(teddywilson) replace with real link
+          onClick={() => {
+            openSocialLink(`https://github.com/wiki-token`);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faTwitter}
+          className="footer-icon"
+          size={FA_ICON_SIZE}
+          onClick={() => {
+            openSocialLink(`https://twitter.com/wiki_token`);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faDiscord}
+          className="footer-icon"
+          size={FA_ICON_SIZE}
+          onClick={() => {
+            openSocialLink(`https://discord.gg/RdrZKjAEyd`);
+          }}
+        />
       </div>
     </Layout.Footer>
   );
