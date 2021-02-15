935cf2ef6243a40ab7611d075f979d66f2e35dd6
bingbongle <contact@defund12.org>
2021-02-15 12:49:00 -0500

    Add warning message

--- packages/frontend/src/components/Layout.jsx
+++ packages/frontend/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Header from "./Header";
+import { Alert } from "antd";
 
 export default function Layout({ address, onConnectWallet, provider, price, children }) {
   return (
     <>
+      <Alert message="Wiki Token is still in development, use at your own risk" type="error" />
       <Header
         address={address}
         onConnectWallet={onConnectWallet}
