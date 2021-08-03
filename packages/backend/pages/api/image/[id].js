0f85e7fd6051b8a8e3a14ccfd5abd2f0caafab40
odd-amphora <odd_amphora@protonmail.com>
2021-08-02 21:25:42 -0400

    Add user agent header?

--- packages/backend/pages/api/image/[id].js
+++ packages/backend/pages/api/image/[id].js
@@ -8,8 +8,21 @@ export default async (req, res) => {
     res.status(404).send(`${req.query.id} not found`);
     return;
   }
-  request.get(queryResponse.imageUrl, { encoding: "binary" }, function (_, response) {
-    res.writeHead(200, { "Content-Type": "image/jpeg", "Cache-Control": "no-cache" });
-    res.end(response.body, "binary");
-  });
+  request.get(
+    queryResponse.imageUrl,
+    {
+      encoding: "binary",
+      headers: {
+        "User-Agent": "WikiToken/0.0 (https://wikitoken.org",
+      },
+    },
+    function (err, response) {
+      if (err) {
+        res.status(500).send("error fetching image");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "image/jpeg", "Cache-Control": "no-cache" });
+      res.end(response.body, "binary");
+    },
+  );
 };
