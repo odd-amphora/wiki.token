94d58845946030a564fa0657204d3f5aa3c36691
odd-amphora <odd_amphora@protonmail.com>
2021-08-02 21:07:15 -0400

    Images? :

--- packages/backend/pages/api/image/[id].js
+++ packages/backend/pages/api/image/[id].js
@@ -1,33 +1,15 @@
 import { fetchPageById } from "../../../utils/query";
-import btoa from "btoa";
 
-// TODO: copied from ENS - implement for wiki
+const request = require("request");
 
-function b64EncodeUnicode(str) {
-  return btoa(
-    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
-      return String.fromCharCode(parseInt(p1, 16));
-    }),
-  );
-}
-
-export function getImage() {
-  const svg =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">' +
-    '<path d="M50,3l12,36h38l-30,22l11,36l-31-21l-31,21l11-36l-30-22h38z" ' +
-    'fill="#FF0" stroke="#FC0" stroke-width="2"/>' +
-    "</svg>";
-  try {
-    return svg;
-  } catch (e) {
-    console.log("err: ", e);
-    return "";
+export default async (req, res) => {
+  let queryResponse = await fetchPageById(req.query.id);
+  if (!queryResponse) {
+    res.status(404).send(`${req.query.id} not found`);
+    return;
   }
-}
-
-export default (req, res) => {
-  res.setHeader("Content-Type", "image/svg+xml");
-  const image = getImage();
-  const body = image;
-  res.send(body);
+  request.get(queryResponse.imageUrl, { encoding: "binary" }, function (_, response) {
+    res.writeHead(200, { "Content-Type": "image/jpeg", "Cache-Control": "no-cache" });
+    res.end(response.body, "binary");
+  });
 };
