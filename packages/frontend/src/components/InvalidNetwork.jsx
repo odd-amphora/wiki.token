75e2fb9e9804b91d80fb84e31d065373afc3cda5
odd-amphora <odd_amphora@protonmail.com>
2021-08-10 09:56:40 -0400

    Errors

--- packages/frontend/src/components/InvalidNetwork.jsx
+++ packages/frontend/src/components/InvalidNetwork.jsx
@@ -26,7 +26,7 @@ export default function InvalidNetwork() {
     if (validNetwork !== isValidNetwork) {
       setIsValidNetwork(validNetwork);
     }
-  }, [account, chainId]);
+  }, [account, chainId, isValidNetwork]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -39,9 +39,10 @@ export default function InvalidNetwork() {
       {isReady && (
         <Modal title="Invalid network" visible={!isValidNetwork} footer={null} closable={false}>
           <span>
-            Your wallet is connected to <a>{NETWORK}</a>, but you are using the{" "}
-            <a>{CHAIN_ID_TO_NETWORK[chainId]}</a> WikiToken app. Please connect to a different
-            network in Metamask, or connect to a different WikiToken application.
+            Your wallet is connected to <span className="invalid-network">{NETWORK}</span>, but you
+            are using the <span className="invalid-network">{CHAIN_ID_TO_NETWORK[chainId]}</span>{" "}
+            WikiToken app. Please connect to a different network in Metamask, or connect to a
+            different WikiToken application.
           </span>
         </Modal>
       )}
