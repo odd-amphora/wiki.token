5046e79f0bd7e11cedab260c5932caf9a55a2034
odd-amphora <odd_amphora@protonmail.com>
2021-08-11 20:52:40 -0400

    Refactor about page

--- packages/frontend/src/views/About.jsx
+++ packages/frontend/src/views/About.jsx
@@ -21,14 +21,6 @@ export default function About() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const AboutLink = ({ link, text, external = true }) => {
-    return (
-      <a target={external === true ? "_blank" : ""} rel="noopener noreferrer" href={link}>
-        {text}
-      </a>
-    );
-  };
-
   return (
     <div className="menu-view" id="about-view">
       <>
@@ -36,48 +28,38 @@ export default function About() {
         <Divider />
         <div className="about-text-block">
           WikiToken is an{" "}
-          <AboutLink
-            link="https://ethereum.org/en/developers/docs/standards/tokens/erc-721/"
-            text="ERC721 Token"
-          />{" "}
-          built on the <AboutLink link="https://ethereum.org" text="Ethereum" /> blockchain,
-          centered on giving back to the{" "}
-          <AboutLink link="https://wikimediafoundation.org/" text="Wikimedia Foundation" />. We are
-          in no way associated with Wikimedia, but we hope, over time, we can transfer this project
+          <a href="https://ethereum.org/en/developers/docs/standards/tokens/erc-721/">
+            ERC721 Token
+          </a>{" "}
+          built on the <a href="https://ethereum.org">Ethereum</a> blockchain, centered on giving
+          back to the <a href="https://wikimediafoundation.org/">Wikimedia Foundation</a>. We are in
+          no way associated with Wikimedia, but we hope, over time, we can transfer this project
           over to them.
         </div>
         <div className="about-text-block">
           Wikimedia provides, unarguably, one of the most important education platforms on the
           internet. Since they are non-proft, they rely on the commmunity at large to keep the
-          organization running. Particularly in the cryptocurrency space, there is an substantial
-          liquidity that can be routed to help organizations like Wikimedia in need. By owning a
-          WikiToken, you are contributing to Wikimedia's vision and sustainability.
+          organization running. By owning a WikiToken, or contributing to the project, you are
+          contributing to Wikimedia's vision and sustainability.
         </div>
       </>
       <>
         <div className="about-text-title">Mechanism</div>
         <Divider />
         <div className="about-text-block">
-          On the site, users can <AboutLink link="/sponsor" text="sponsor" external="false" />, or
-          "mint", WikiToken(s) with Wikipedia Page URL(s). We have built an API that resolves each
-          Page URL to a{" "}
-          <AboutLink
-            link="https://www.mediawiki.org/wiki/Manual:Page_table#page_id"
-            text="Page ID"
-          />{" "}
-          via{" "}
-          <AboutLink link="https://www.mediawiki.org/wiki/API:Main_page" text="Wikimedia's API" />.
-          The Page ID is what is ultimately used to ensure that each token is unique.
+          On the site, users can <a href="/sponsor">sponser</a>, or "mint", WikiToken(s) with
+          Wikipedia Page URL(s). We have built an API that resolves each Page URL to a{" "}
+          <a href="https://www.mediawiki.org/wiki/Manual:Page_table#page_id">Page ID</a> via{" "}
+          <a href="https://www.mediawiki.org/wiki/API:Main_page">Wikimedia's API</a>. The Page ID is
+          what is ultimately used to ensure that each token is unique.
         </div>
         <div className="about-text-block">
-          Each WikiToken costs a flat rate of {MINT_FEE} to mint. This fee is routed to the
-          WikiTokenDAO Juicebox project, which holds the WikiToken treasury; recurring funding
-          targets are set to pay mainters of the project, and overflow funds will be routed to
-          Wikimedia. We anticipate the vast majority of the treasury to live in overflow.
-        </div>
-        <div className="about-text-block">
-          Secondary sales on <AboutLink link="https://opensea.io" text="Open Sea" /> will incur a
-          small royalty fee that will be routed to the same treasury.
+          Each WikiToken costs a flat rate of {MINT_FEE} to mint. This fee is routed to the{" "}
+          <a href="https://juicebox.money/#/p/wikitokendao">WikiTokenDAO Juicebox project</a>, which
+          holds the WikiToken treasury and funding cycle configuration(s). Secondary sales on{" "}
+          <a href="https://opensea.io">Open Sea</a> will incur a small royalty fee (10%) that will
+          be routed to the same treasury. This configuration is subject to change as the protocol
+          and DAO evolve, but we have set an initial target of 10,000 ETH over the next 100 days.
         </div>
       </>
       <>
@@ -92,13 +74,14 @@ export default function About() {
         </div>
         <div className="about-text-block">
           Currently, the WikiTokenDAO Juicebox project and treasury is owned by{" "}
-          <AboutLink
-            link="https://etherscan.io/address/0x549238d4ee184e2cb4d3b7dcf47be933d1348fa8"
-            text="wikitoken.eth"
-          />
+          <a href="https://etherscan.io/address/0x549238d4ee184e2cb4d3b7dcf47be933d1348fa8">
+            wikitoken.eth
+          </a>
           , a multisig wallet shared by the creators of WikiToken, as well as other projects in the
-          crypto space; some of these include <AboutLink link="https://juice.money" text="Juice" />,{" "}
-          <AboutLink link="https://tokentax.co" text="TokenTax" />, and more.
+          crypto space; some of these include <a link="https://juice.money">Juice</a>,{" "}
+          <a href="https://tokentax.co">Token Tax</a>, and more. Immediately after the protocol is
+          launched, we plan on adding more members of the DAO to the multisig to enforce stronger
+          governance.
         </div>
       </>
       <>
@@ -106,9 +89,7 @@ export default function About() {
         <Divider />
         <div className="about-text-block">
           There are currently {totalSupply} WikiTokens in circulation. Supply is expected to grow
-          over time as new Wikipedia pages are created. However, we cannot control what happens to
-          tokens if a page is removed by Wikimedia. This is rare anyway, so we don't expect it to be
-          an issue.
+          over time as new Wikipedia pages are created.
         </div>
       </>
       <>
@@ -117,12 +98,11 @@ export default function About() {
         <div className="about-text-block">
           WikiToken is 100% open source. We are always looking for new members to join our
           community, each with the opportunity to contribute in however they please. If you're
-          looking to get started, joining our{" "}
-          <AboutLink link="https://discord.gg/RdrZKjAEyd" text="discord" /> community is a good
-          place to begin. If you've found an issue with the site, please feel free to bring it up in
-          the relevant channel there, or (even better) just{" "}
-          <AboutLink link="https://github.com/odd-amphora/wiki.token/issues" text="open an issue" />{" "}
-          directly on Github. Thank you ❤️
+          looking to get started, joining our <a href="https://discord.gg/RdrZKjAEyd">Discord</a>{" "}
+          community is a good place to begin. If you've found an issue with the site, please feel
+          free to bring it up in the relevant channel there, or (even better) just{" "}
+          <a href="https://github.com/odd-amphora/wiki.token/issues">open an issue</a> directly on
+          Github. Thank you ❤️
         </div>
       </>
     </div>
