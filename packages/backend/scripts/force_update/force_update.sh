51ddf53ef4bde15d3167bb429bcd460eb85c4fe0
odd-amphora <odd_amphora@protonmail.com>
2021-08-13 09:22:29 -0400

    Edit

--- packages/backend/scripts/force_update/force_update.sh
+++ packages/backend/scripts/force_update/force_update.sh
@@ -5,4 +5,5 @@ while [ $i -le 1000000000000 ]; do
   url="https://api.opensea.io/asset/0xD224B0eAf5B5799ca46D9FdB89a2C10941E66109/${i}/?force_update=true"
   curl ${url}
   sleep 1
+  i=$(( i + 1 ))
 done
